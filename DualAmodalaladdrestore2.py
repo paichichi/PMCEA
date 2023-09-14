afada35da06d9ee0d7d91956c3aa4f0b07370fbc
LoveBigAI <141302972+LoveBigAI@users.noreply.github.com>
2023-09-14 11:37:03 +0800

    Add files via upload

---
+++ DualAmodalaladdrestore2.py
@@ -0,0 +1,355 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import os
+# import keras
+import numpy as np
+from utils import *
+from tqdm import *
+# from evaluate import evaluate
+from eval import evaluateTorch
+import tensorflow as tf
+# import keras.backend as K
+# from keras.layers import *
+from models import *
+import torch.nn.functional as F
+import torch
+from models_encodercat import Encoder_Model
+from models_encodercatal import Encoder_Model_Al
+import random
+
+
+def set_seed(seed, cuda=True):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+# tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+#config = tf.ConfigProto()
+#config.gpu_options.allow_growth=True  
+#sess = tf.Session(config=config)  
+
+seed =12306 
+np.random.seed(seed)
+
+set_seed(seed)
+tf.compat.v1.set_random_seed(seed)
+
+
+data_ca="ja_en/"
+# train_pair,dev_pair,adj_matrix,r_index,r_val,adj_features,rel_features = load_data("data/fb_yago_en/",train_ratio=0.20)
+data_dir = 'data/'+data_ca
+_,_,_,adj_matrix,r_index,r_val,adj_features,rel_features = load_data(data_dir,train_ratio=0.30)
+
+output_dir="checkpoints/"+data_ca
+
+ref_pairs = np.load(os.path.join(output_dir, "ref_pairs.npz"))
+train_pair = ref_pairs["train_pair"]
+val_pair = ref_pairs["valid_pair"]
+dev_pair = ref_pairs["dev_pair"]
+
+used_inds2_array = np.load(os.path.join(output_dir, "used_inds2.npz"))
+used_inds2_array=used_inds2_array['used_inds2']
+used_inds=used_inds2_array.tolist()
+
+visual_links_array2 = np.load(os.path.join(output_dir, "visual_links_array2.npz"))
+visual_links_array2 = visual_links_array2['visual_links_array2']
+visual_links = visual_links_array2.tolist()
+
+
+adj_matrix = np.stack(adj_matrix.nonzero(),axis = 1)
+rel_matrix,rel_val = np.stack(rel_features.nonzero(),axis = 1),rel_features.data
+ent_matrix,ent_val = np.stack(adj_features.nonzero(),axis = 1),adj_features.data
+
+
+
+
+node_size = adj_features.shape[0]
+rel_size = rel_features.shape[1]
+triple_size = len(adj_matrix)
+node_hidden = 128
+rel_hidden = 128
+batch_size = 1024
+dropout_rate = 0.5
+lr = 0.005
+gamma = 1
+depth = 2
+
+device = "cuda:0"
+
+
+
+
+
+
+# img_features = load_img_features(node_size, "data/fb_dbk_en/FB15K_DB15K")
+# img_features = load_img_features(node_size, "data/fb_dbk_en/FB15K_YAGO15K")
+img_features = load_img_features(node_size, data_dir)
+img_features = F.normalize(torch.Tensor(img_features).to(device))
+print("image feature shape:", img_features.shape)
+
+
+triple_size = len(adj_matrix)  # not triple size, but number of diff(h, t)
+eval_epoch = 3
+
+
+
+training_time = 0.
+grid_search_time = 0.
+time_encode_time = 0.
+
+ 
+e1 = os.path.join(data_dir,'ent_ids_1')
+e2 = os.path.join(data_dir,'ent_ids_2')
+left_ents = get_ids(e1)
+right_ents = get_ids(e2)
+
+unsup=True
+unsup_k=6000
+
+
+
+# visual_links=[]
+# used_inds = []
+
+l_img_f = img_features[left_ents] # left images
+r_img_f = img_features[right_ents] # right images
+
+img_sim = l_img_f.mm(r_img_f.t())
+topk =unsup_k
+two_d_indices,vals = get_topk_indices(img_sim, topk*100)
+del l_img_f, r_img_f, #img_sim
+
+l_img_dev = img_features[dev_pair[:,0]]
+r_img_dev = img_features[dev_pair[:,1]]
+img_sim_dev = l_img_dev.mm(r_img_dev.t())
+
+
+l_img_val = img_features[val_pair[:,0]]
+r_img_val = img_features[val_pair[:,1]]
+img_sim_val = l_img_val.mm(r_img_val.t())
+
+# count = 0
+# for index in range(len(two_d_indices)):
+#     ind = two_d_indices[index]
+#     if left_ents[ind[0]] in used_inds: continue
+#     if right_ents[ind[1]] in used_inds: continue
+#     used_inds.append(left_ents[ind[0]])
+#     used_inds.append(right_ents[ind[1]])
+#     visual_links.append((left_ents[ind[0]], right_ents[ind[1]],vals[index].cpu(),vals[index].cpu()))
+#     # visual_links.append((left_ents[ind[0]], right_ents[ind[1]]))
+
+#     count += 1
+#     if count == topk: 
+#         break
+
+
+# num_al=1500
+# count_al=0
+# for index in range(len(train_pair)):
+#     ind = train_pair[index]
+#     if ind[0] in used_inds: continue
+#     if ind[1] in used_inds: continue
+#     visual_links.append((ind[0], ind[1],1,1))
+#     count_al +=1
+#     if count_al==num_al:
+#         break
+
+
+# for index in range(1500):
+#     ind = train_pair[index]
+#     visual_links.append((ind[0], ind[1],1,1))
+
+train_pair_base=train_pair
+
+
+print('begin')
+# inputs = [adj_input, index_input, val_input, rel_adj, ent_adj]
+inputs = [adj_matrix, r_index, r_val, rel_matrix, ent_matrix]
+# model = OverAll(node_size=node_size, node_hidden=node_hidden,
+#                 rel_size=rel_size, rel_hidden=rel_hidden,
+#                 rel_matrix=rel_matrix, ent_matrix=ent_matrix,
+#                 triple_size=triple_size, dropout_rate=dropout_rate,
+#                 depth=depth, device=device)
+# model = model.to(device)
+
+adj_matrix = torch.from_numpy(np.transpose(adj_matrix))
+rel_matrix = torch.from_numpy(np.transpose(rel_matrix))
+ent_matrix = torch.from_numpy(np.transpose(ent_matrix))
+r_index = torch.from_numpy(np.transpose(r_index))
+r_val = torch.from_numpy(r_val)
+
+
+entity_label = np.zeros((node_size,node_hidden))
+for item in visual_links:
+    entity_label[item[0]]=np.array([1]*node_hidden)
+    entity_label[item[1]]=np.array([1]*node_hidden)
+
+model_al = Encoder_Model_Al(node_size=node_size, node_hidden=node_hidden,
+                rel_size=rel_size, rel_hidden=rel_hidden,new_node_size=node_size,
+                rel_matrix=rel_matrix, ent_matrix=torch.tensor(entity_label),r_index=r_index,r_val=r_val,
+                adj_matrix=adj_matrix,img_feature = None,
+                triple_size=triple_size, dropout_rate=dropout_rate,
+                depth=depth, device=device)
+model_al = model_al.to(device)
+
+
+
+
+if os.path.exists(os.path.join(output_dir, "check_model2.tar")):
+    # self.model.load_weights(os.path.join(from_dir, "model.ckpt"))
+    pretrained_dict = torch.load(os.path.join(output_dir, "check_model2.tar"))['state_dict']
+    model_dict = model_al.state_dict()
+
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in {"rel_embedding.weight", "e_encoder.attn_kernels.0","e_encoder.attn_kernels.1"}}
+    print(pretrained_dict.keys())
+    model_dict.update(pretrained_dict)
+    model_al.load_state_dict(model_dict)
+else:
+    print("can not load previouse model")
+
+model_al.eval()
+with torch.no_grad():
+     train_paire_al = model_al(train_pair_base).mean(-1)
+
+sorted, indices = torch.sort(train_paire_al) 
+
+selected_train=train_pair[indices.cpu()]
+
+num_al=500
+count_al=0
+for index in range(len(selected_train)):
+    ind = selected_train[index]
+    if ind[0] in used_inds: continue
+    if ind[1] in used_inds: continue
+    used_inds.append(ind[0])
+    used_inds.append(ind[1])
+    visual_links.append((ind[0], ind[1],1,1))
+    count_al +=1
+    if count_al==num_al:
+        break
+
+train_pair = np.array(visual_links, dtype=np.float32)
+
+
+
+
+model = Encoder_Model(node_size=node_size, node_hidden=node_hidden,
+                rel_size=rel_size, rel_hidden=rel_hidden,new_node_size=node_size,
+                rel_matrix=rel_matrix, ent_matrix=torch.tensor(ent_matrix),r_index=r_index,r_val=r_val,
+                adj_matrix=adj_matrix,img_feature = img_features,
+                triple_size=triple_size, dropout_rate=dropout_rate,
+                depth=depth, device=device)
+model = model.to(device)
+# opt = torch.optim.RMSprop(model.parameters(), lr=lr)
+opt = torch.optim.RMSprop(model.parameters(), lr=lr, weight_decay=0.0005)
+print('model constructed')
+
+
+
+# evaluater = evaluate(dev_pair)
+evaluater_torch = evaluateTorch(device,dev_pair)
+evaluater_torch_val = evaluateTorch(device,val_pair)
+rest_set_1 = [e1 for e1, e2 in dev_pair]
+rest_set_2 = [e2 for e1, e2 in dev_pair]
+np.random.shuffle(rest_set_1)
+np.random.shuffle(rest_set_2)
+
+model.eval()
+with torch.no_grad():
+    # dev_pair = torch.from_numpy(dev_pair).to(device)
+    Lvec, Rvec = model.get_embeddings(dev_pair[:, 0], dev_pair[:, 1],turn=0)
+    # output = model(inputs)
+    # Lvec, Rvec = get_embedding(dev_pair[:, 0], dev_pair[:, 1], output.cpu())
+    # evaluater.test(Lvec, Rvec)
+    evaluater_torch.test(Lvec, Rvec,img_sim_dev.cpu(),img_sim_dev.t().cpu())
+epoch = 12
+
+for turn in range(5):
+
+    for i in trange(epoch):
+        model.train()
+        np.random.shuffle(train_pair)
+        for pairs in [train_pair[i * batch_size:(i + 1) * batch_size] for i in
+                        range(len(train_pair) // batch_size + 1)]:
+            inputs = [adj_matrix, r_index, r_val, rel_matrix, ent_matrix]
+            # output = model(inputs)
+            # loss = align_loss(pairs, output)
+            pairs = torch.from_numpy(pairs).to(device)
+            loss  =model(pairs,turn)
+            # print(loss)
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+        # if i == epoch - 1:
+            # toc = time.time()
+        model.eval()
+        with torch.no_grad():
+            Lvec, Rvec = model.get_embeddings(dev_pair[:, 0], dev_pair[:, 1],turn)
+            Lvec_val, Rvec_val = model.get_embeddings(val_pair[:, 0], val_pair[:, 1],turn)
+            # output = model(inputs)
+            # Lvec, Rvec = get_embedding(dev_pair[:, 0], dev_pair[:, 1], output.cpu())
+            # evaluater.test(Lvec, Rvec)
+            evaluater_torch.test(Lvec, Rvec,img_sim_dev.cpu(),img_sim_dev.t().cpu())
+            evaluater_torch_val.test(Lvec_val, Rvec_val,img_sim_val.cpu(),img_sim_val.t().cpu())
+            # output2 = output.cpu().numpy()
+            # output2 = output2 / (np.linalg.norm(output2, axis=-1, keepdims=True) + 1e-5)
+            # dto.saveobj(output2, 'embedding_of_' + save_suffix())
+  
+
+        new_pair = []
+
+    l_img_dev_boot = img_features[rest_set_1]
+    r_img_dev_boot = img_features[rest_set_2]
+    img_sim_dev_boot = l_img_dev_boot.mm(r_img_dev_boot.t())
+
+    Lvec,Rvec = model.get_embeddings(rest_set_1,rest_set_2,turn)
+    # A,B,A_score,B_score = evaluater.CSLS_cal(Lvec.detach().numpy(),Rvec.detach().numpy(),False)
+    A,B = evaluater_torch.CSLS_cal(Lvec.detach(),Rvec.detach(),img_sim_dev_boot.cpu(),img_sim_dev_boot.t().cpu(),False)
+
+    
+    for i,j in enumerate(A):
+        if  B[j] == i:
+            # new_pair.append([rest_set_1[j],rest_set_2[i],A_score[i],B_score[j]])
+            new_pair.append([rest_set_1[j],rest_set_2[i],1,1])
+            # new_pair_value.append([A_score[j],B_score[i]])  # 不清楚对不对
+    A_score_sorted = np.sort(np.array(new_pair)[:,2])
+    print ("highest A_score_sorted:", A_score_sorted[-1].item(), "lowest A_score_sorted:", A_score_sorted[0].item())
+
+    B_score_sorted = np.sort(np.array(new_pair)[:,3])
+    print ("highest B_score_sorted:", B_score_sorted[-1].item(), "lowest B_score_sorted:", B_score_sorted[0].item())
+
+
+
+    train_pair = np.concatenate([train_pair,np.array(new_pair)],axis = 0)
+    # train_pair_score = np.concatenate([train_pair_score,np.array(new_pair_value)],axis = 0)
+    for e1,e2,_,_ in new_pair:
+        if e1 in rest_set_1:
+            rest_set_1.remove(e1) 
+        
+    for e1,e2,_,_ in new_pair:
+        if e2 in rest_set_2:
+            rest_set_2.remove(e2)
+    epoch = 5
+    
+
+torch.save({'state_dict': model.state_dict()}, os.path.join(output_dir, "check_model3.tar"))
+visual_links_array = np.array(visual_links, dtype=np.int32)
+np.savez(os.path.join(output_dir, "visual_links_array3.npz"), visual_links_array3=visual_links_array)
+used_inds_array = np.array(used_inds, dtype=np.int32)
+np.savez(os.path.join(output_dir, "used_inds3.npz"), used_inds3=used_inds_array)
+
+
+
+
+
+
---
+++ eval.py
@@ -0,0 +1,133 @@
+import torch
+import logging
+import numpy as np
+
+
+class evaluateTorch:
+    def __init__(self,  device, dev_pair,eval_batch_size=1024, k=10, batch=1024):
+        self.device = device
+        self.dev_pair = dev_pair
+        self.eval_batch_size = eval_batch_size
+        self.topk = k
+        self.batch = batch
+
+    def sim_results(self, Matrix_A, Matrix_B):
+        # A x B.t
+        A_sim = torch.mm(Matrix_A, Matrix_B.t())
+        return A_sim
+
+
+
+    def avg_results(self, Matrix_A):
+        k = 10
+        avg_results = torch.sum(torch.topk(Matrix_A, k=k)[0], dim=-1) / k
+        return avg_results
+
+    def CSLS_results(self, inputs):
+        SxT_sim, TxS_avg, SxT_avg,ans_rank = inputs
+
+        TxS_avg, SxT_avg = [torch.unsqueeze(m, dim=1) for m in [TxS_avg, SxT_avg]]
+        sim = 2 * SxT_sim - SxT_avg - TxS_avg.t()
+        # rank = torch.argsort(sim, dim=-1, descending=True)
+
+        # targets = rank[:, :self.topk]
+        # values = torch.gather(sim, 1, targets)
+
+        #     rank = tf.argsort(-sim,axis=-1)
+        #     results = tf.where(tf.equal(rank,K.cast(K.tile(ans_rank,[1,len(self.dev_pair)]),dtype="int32")))
+        #     return K.expand_dims(results,axis=0)
+        # results = torch.where(torch.equal(rank, torch.tensor(ans_rank).unsqueeze(0).repeat(1, len(self.dev_pair)).int()))
+        rank = torch.argsort(-sim, dim=-1)
+        results = torch.where(torch.eq(rank, torch.tensor(ans_rank).unsqueeze(1).repeat(1, len(self.dev_pair)).to(self.device)))
+        return results[1]
+        # return torch.unsqueeze(results[1], dim=0)
+
+        # return targets.cpu().numpy(), values.cpu().numpy()
+
+    def CSLS_rank(self, inputs):
+        SxT_sim, TxS_avg, SxT_avg,ans_rank = inputs
+
+        TxS_avg, SxT_avg = [torch.unsqueeze(m, dim=1) for m in [TxS_avg, SxT_avg]]
+        sim = 2 * SxT_sim - SxT_avg - TxS_avg.t()
+        # rank = torch.argsort(sim, dim=-1, descending=True)
+
+        # targets = rank[:, :self.topk]
+        # values = torch.gather(sim, 1, targets)
+
+        #     rank = tf.argsort(-sim,axis=-1)
+        #     results = tf.where(tf.equal(rank,K.cast(K.tile(ans_rank,[1,len(self.dev_pair)]),dtype="int32")))
+        #     return K.expand_dims(results,axis=0)
+        # results = torch.where(torch.equal(rank, torch.tensor(ans_rank).unsqueeze(0).repeat(1, len(self.dev_pair)).int()))
+        rank = torch.argsort(-sim, dim=-1)
+     
+        return rank[:,0]
+
+    def CSLS_cal(self, sourceVec, targetVec,m1,m2,evaluate=True):
+        batch_size = self.eval_batch_size
+        SxT_sim = []
+        TxS_sim=[]
+        for epoch in range(len(sourceVec) // batch_size + 1):
+            SxT_sim.append(self.sim_results(sourceVec[epoch * batch_size:(epoch + 1) * batch_size], targetVec))
+            TxS_sim.append(self.sim_results(targetVec[epoch * batch_size:(epoch + 1) * batch_size], sourceVec))
+
+        alpha=0.2
+        # if evaluate:
+
+        for epoch in range(len(sourceVec) // batch_size + 1):
+            SxT_sim[epoch]=SxT_sim[epoch]*(1-alpha)+m1[epoch * batch_size:(epoch + 1) * batch_size,:]*alpha
+            TxS_sim[epoch]=TxS_sim[epoch]*(1-alpha)+m2[epoch * batch_size:(epoch + 1) * batch_size,:]*alpha
+
+
+        SxT_avg = []
+        TxS_avg = []
+        for epoch in range(len(sourceVec) // batch_size + 1):
+            SxT_avg.append(self.avg_results(SxT_sim[epoch]))
+            TxS_avg.append(self.avg_results(TxS_sim[epoch]))
+
+        # SxT_sim = self.sim_results(sourceVec, targetVec)
+        # SxT_sim = SxT_sim*(1-alpha)+m*alpha
+
+        # SxT_avg = self.avg_results(SxT_sim)
+
+        # TxS_sim = self.sim_results(targetVec, sourceVec)
+        # TxS_sim = TxS_sim*(1-alpha)+m*alpha
+
+        # TxS_avg = self.avg_results(TxS_sim)
+
+
+        if evaluate:
+            targets = np.empty((0, self.topk), int)
+            targets=[]
+            for epoch in range(len(sourceVec) // batch_size + 1):
+                ans_rank = np.array([i for i in range(epoch * batch_size,min((epoch+1) * batch_size,len(sourceVec)))])
+                temp_targets = self.CSLS_results([SxT_sim[epoch].to(device=self.device),torch.cat((TxS_avg),dim=0).to(device=self.device), SxT_avg[epoch].to(device=self.device),ans_rank])
+                # targets = np.concatenate((targets, temp_targets), axis=0)
+                targets.append(temp_targets)
+                # values = np.concatenate((values, temp_values), axis=0)
+            return torch.cat((targets),0)
+        else :
+            l_rank,r_rank = [],[]
+            for epoch in range(len(sourceVec) // batch_size + 1):
+                ans_rank = np.array([i for i in range(epoch * batch_size,min((epoch+1) * batch_size,len(sourceVec)))])
+                l_rank.append(self.CSLS_rank([SxT_sim[epoch].to(device=self.device), torch.cat((TxS_avg),dim=0).to(device=self.device), SxT_avg[epoch].to(device=self.device),ans_rank]))
+                r_rank.append(self.CSLS_rank([TxS_sim[epoch].to(device=self.device), torch.cat((SxT_avg),dim=0).to(device=self.device), TxS_avg[epoch].to(device=self.device),ans_rank]))
+
+            # return np.concatenate(r_rank,axis=1)[0],np.concatenate(l_rank,axis=1)[0] 
+            return  torch.cat((r_rank),dim=0).cpu().numpy(),torch.cat((l_rank),dim=0).cpu().numpy()
+        
+    def test(self, Lvec,Rvec,m1,m2):
+        results  = self.CSLS_cal(Lvec,Rvec,m1,m2)
+        def cal(results):
+            hits1,hits5,hits10,mrr = 0,0,0,0
+            for x in results:
+                if x < 1:
+                    hits1 += 1
+                if x < 5:
+                    hits5 += 1
+                if x < 10:
+                    hits10 += 1
+                mrr += 1/(x + 1)
+            return hits1,hits5,hits10,mrr
+        hits1,hits5,hits10,mrr = cal(results)
+        print("Hits@1: ",hits1/len(Lvec)," ","Hits@5: ",hits5/len(Lvec)," ","Hits@10: ",hits10/len(Lvec)," ","MRR: ",mrr/len(Lvec))
+        return results
